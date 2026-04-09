d4169c3ae645f6cc05380be0b8c616c5ab8ad693
Zhou Hao <zhmsolone@outlook.com>
2026-04-09 16:04:59 +0800

    feat: Add function MiniMindBlock().

--- model/model.py
+++ model/model.py
@@ -305,7 +305,7 @@ def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
             .reshape(bs, slen, num_key_value_heads * n_rep, head_dim)
 
 
-class attention(nn.Module):
+class Attention(nn.Module):
     def __init__(self, config: MiniMindConfig):
         super().__init__()
         self.num_key_value_heads = config.num_attention_heads \
@@ -469,3 +469,32 @@ class FeedForward(nn.Module):
             )
 
 
+# Transformer Block
+class MiniMindBlock(nn.Module):
+    def __init__(self, layer_id: int, config: MiniMindConfig):
+        super().__init__()
+        self.self_attn = Attention(config)
+        self.input_layernorm = RMSNorm(config.hidden_size, eps = config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(config.hidden_size, eps = config.rms_norm_eps)
+        self.mlp = FeedForward(config) # if not config.use_moe else MoEFeedForward(config)
+
+    def forward(self, 
+                hidden_states, 
+                position_embeddings, 
+                past_key_value = None, 
+                use_cache = False, 
+                attention_mask = None):
+        residual = hidden_states
+
+        # Self-Attention
+        hidden_states, past_key_value = self.self_attn(self.input_layernorm(hidden_states), 
+                                                        position_embeddings,
+                                                        past_key_value,
+                                                        use_cache, 
+                                                        attention_mask)
+        hidden_states += residual
+
+        # FeedForward
+        hidden_states = hidden_states + self.mlp(self.post_attention_layernorm(hidden_states))
+
+        return hidden_states, past_key_value
